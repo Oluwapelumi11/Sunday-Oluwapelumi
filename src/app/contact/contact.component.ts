4cd9346dea71eef04b275d08380f8f183d0c7152
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2023-12-12 06:18:36 +0100

    contact animations

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { CommonModule} from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser} from '@angular/common';
 import { FormGroup,FormControl,ReactiveFormsModule,Validators } from '@angular/forms';
 import { ContactService } from '../contact.service';
+import Typewriter from 't-writer.js';
 
 
 @Component({
@@ -20,10 +21,13 @@ export class ContactComponent {
     email : new FormControl('',[Validators.required]),
     title : new FormControl('',[Validators.required]),
     message : new FormControl('',[Validators.required,Validators.minLength(8)]),
-
+    
   });
+ 
 
-  constructor(private contactService: ContactService){ }
+  constructor(private contactService: ContactService,
+    @Inject(PLATFORM_ID) private platformId: Object
+    ){ }
   onSubmit(){
     if (this.contactForm.valid){
       this.buttonValue = "Sending..."
@@ -32,11 +36,36 @@ export class ContactComponent {
       this.contactForm.reset
       this.submitSuccess= true
       this.buttonValue = "Send"
+      if (isPlatformBrowser(this.platformId)) {
+        this.Writer();
+      }
       setTimeout(() => {
         this.submitSuccess = false
-      }, 5000)
+      }, 4000)
     })
     }
     
+  }
+
+  Writer(): void {
+    const target = document.querySelector(".success");
+    const options = {
+      loop: false,
+      typeColor: 'white',
+      cursorColor: 'white',
+      typeSpeed: 'random',
+    };
+    const writer = new Typewriter(target, options);
+
+    writer
+ 
+    .type('Message Sent Successfully!')
+    .rest(2500)
+    .clear()
+    .removeCursor()
+    .start()
+  }
+  better(): void {
+    this.Writer()
   }
 }
