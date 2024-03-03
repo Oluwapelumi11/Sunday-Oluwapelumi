b82db92366dad67b9ba8be9247e2159f8f1e71f7
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2024-03-03 12:13:38 +0100

    fixed rxjs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+import { withViewTransitions } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),provideHttpClient(withFetch()), provideClientHydration()],
+  providers: [provideRouter(routes),provideHttpClient(withFetch()), provideClientHydration(),withViewTransitions()],
 
 };
